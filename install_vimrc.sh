88eab2bced3e27a90dab8bd39c0329d31f5b3fdc
Martin Ortbauer <mortbauer@gmail.com>
2013-09-11 11:06:46 +0200

    bug with escaping fixed

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -17,5 +17,6 @@ echo 'export XDG_CONFIG_HOME=$HOME/.config' >> "${HOME}/.zshrc"
 echo 'export XDG_CONFIG_HOME=$HOME/.config' >> "${HOME}/.bashrc" 
 echo 'export XDG_CACHE_HOME=$HOME/.cache' >>  "${HOME}/.zshrc" 
 echo 'export XDG_CACHE_HOME=$HOME/.cache' >>  "${HOME}/.bashrc" 
-echo export VIMINIT=' let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC' >> "${HOME}/.zshrc" 
-echo export VIMINIT=' let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC' >> "${HOME}/.bashrc" 
+# ansi quoting, see: http://stackoverflow.com/a/8802512/1607448
+echo $'export VIMINIT=\' let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC\'' >> "${HOME}/.zshrc" 
+echo $'export VIMINIT=\' let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC\'' >> "${HOME}/.bashrc" 
